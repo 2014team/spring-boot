a7550c3da979f88a67855b74e27b6a6fdecf9bf6
Administrator <645874679@qq.com>
2019-08-10 11:51:07 +0800

    自定义starter

---
+++ 09-usecustomstarter/src/main/java/com/abc/controller/SomeController.java
@@ -0,0 +1,18 @@
+package com.abc.controller;
+
+import com.abc.service.SomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeController {
+    @Autowired   // byType方式的自动注入
+    private SomeService service;
+
+    @RequestMapping("/some/{param}")
+    public String someHandle(@PathVariable("param") String param) {
+        return service.wrap(param);
+    }
+}
